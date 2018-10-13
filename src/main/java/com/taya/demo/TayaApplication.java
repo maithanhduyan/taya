ae1e8ae56238d9586938a48e261eb9ccc4a670dc
Mai Thành Duy An <tiachop0102@gmail.com>
2018-10-14 00:50:08 +0700

    Create Spring Boot Project

---
+++ src/main/java/com/taya/demo/TayaApplication.java
@@ -0,0 +1,12 @@
+package com.taya.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TayaApplication.class, args);
+	}
+}
