8a9c99c639655f251d01202604ea583ccd846f62
Nguyen Ly Minh Man <nguyenlyminhman@gmail.com>
2018-10-28 14:21:38 +0700

    modify sql file

--- sql/create/create-table-v1.0.sql
+++ sql/create/create-table-v1.0.sql
@@ -3,126 +3,88 @@ database design here !
 -- -----------------------------------------------------
 -- Table users
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS users(
-	idUser serial primary key,
-	first_name VARCHAR(50), 
-    last_name VARCHAR(50), 
+CREATE TABLE IF NOT EXISTS Users(
+	userId BIGINT PRIMARY KEY,
+    userUuid VARCHAR(255) UNIQUE,
+	firstName VARCHAR(50), 
+    lastName VARCHAR(50), 
     dob DATE,
 	email VARCHAR(50), 
-	password VARCHAR(50), 
+	password VARCHAR(150), 
 	phone VARCHAR(50), 
 	address VARCHAR(150)
-)
+);
 -- -----------------------------------------------------
 -- Table   Category 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS Category  (
-   idCategory  serial primary key,
-   category_name  VARCHAR(50),
-   category_description  VARCHAR(1000)
-)
+   categoryId  BIGINT PRIMARY KEY,
+   categoryName  VARCHAR(150),
+   description  VARCHAR(1000)
+);
 -- -----------------------------------------------------
 -- Table   Product 
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS Product  (
-   idProduct  serial primary key,
-   idCategory  INT,
-   product_name  VARCHAR(50),
-   product_price  numeric,
-   product_description  VARCHAR(1000),
-   product_quantity  INT,
-   CONSTRAINT  fk_Product_Category
-   FOREIGN KEY (idCategory) REFERENCES   Category  ( idCategory )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
+CREATE TABLE Product(
+	productId BIGINT PRIMARY KEY,
+    productUuid VARCHAR(255) UNIQUE,
+	productName VARCHAR(150)  NOT NULL UNIQUE,
+	productNumber VARCHAR(255) NOT NULL UNIQUE,
+	productCode VARCHAR(32) NOT NULL UNIQUE,
+	makeFlag BOOLEAN,
+	finishGoodFlags BOOLEAN,
+	color VARCHAR(50) ,
+	safetyStockLever INT,
+	standardCost INT
+);
 -- -----------------------------------------------------
 -- Table   Customer 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS Customer  (
-   idCustomer  serial primary key,
-   first_name  VARCHAR(50),
-   last_name  VARCHAR(50),
+   customerId BIGINT PRIMARY KEY,
+   firstName  VARCHAR(50),
+   lastName  VARCHAR(50),
    email  VARCHAR(50),
    password  VARCHAR(50),
    address  VARCHAR(150),
    dob  DATE
-)
+);
 -- -----------------------------------------------------
 -- Table   Order 
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS orders  (
-   idOrder VARCHAR(50) primary key,
-    idCustomer  INT NOT NULL,
-   order_date  DATE,
-   order_description  VARCHAR(1000),
-  CONSTRAINT  fk_Order_Customer 
-    FOREIGN KEY ( idCustomer )
-    REFERENCES   Customer  ( idCustomer )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
+CREATE TABLE IF NOT EXISTS Orders  (
+   orderId BIGINT PRIMARY KEY,
+   orderUuid VARCHAR(255) UNIQUE,
+   orderDate  DATE,
+   description  VARCHAR(1000)
+);
 -- -----------------------------------------------------
 -- Table   Image 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS Image (
-   idImage  serial primary key,
-   idProduct  INT NOT NULL,
-   img_name  VARCHAR(100),
-   img_data  bytea ,
-   CONSTRAINT  fk_Image_Product 
-   FOREIGN KEY ( idProduct)
-   REFERENCES   Product  ( idProduct)
-   ON DELETE NO ACTION
-   ON UPDATE NO ACTION
-)
+   imageId  BIGINT PRIMARY KEY,
+   imageName  VARCHAR(100),
+   imageData  bytea 
+);
 -- -----------------------------------------------------
 -- Table   OrderDetails 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS   OrderDetails  (
-   idOrderDetails  serial primary key,
+   orderDetailsId BIGINT PRIMARY KEY,
    quantity  INT,
-   price  NUMERIC,
-   idOrder  VARCHAR(50) NOT NULL,
-   idCustomer  INT NOT NULL,
-   idProduct  INT NOT NULL,
-  CONSTRAINT  fk_OrderDetails_Order
-    FOREIGN KEY ( idOrder )
-    REFERENCES   Orders  ( idOrder )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT  fk_OrderDetails_Product
-    FOREIGN KEY ( idProduct )
-    REFERENCES   Product  ( idProduct )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
+   price  NUMERIC
+);
 -- -----------------------------------------------------
 -- Table   SpecialOffer 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS   SpecialOffer  (
-   idSpecialOffer serial primary key,
+   specialOfferId BIGINT PRIMARY KEY,
    description  VARCHAR(1000),
    percent  INT
-)
+);
 -- -----------------------------------------------------
 -- Table   SpecialOfferProduct 
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS   SpecialOfferProduct  (
-   idSpecialOfferProduct serial primary key,
-   idSpecialOffer  INT NOT NULL,
-   idProduct  INT NOT NULL,
-   CONSTRAINT  fk_SpecialOfferProduct_SpecialOffer
-    FOREIGN KEY ( idSpecialOffer )
-    REFERENCES   SpecialOffer  ( idSpecialOffer )
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-    CONSTRAINT  fk_SpecialOfferProduct_Product
-    FOREIGN KEY ( idProduct )
-    REFERENCES   Product  ( idProduct)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-)
-
-
-
+   specialOfferProductId BIGINT PRIMARY KEY
+);
