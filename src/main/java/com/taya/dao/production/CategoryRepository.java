94e0c183e38b29c914656ac2c5769a17d4a1e2b8
Nguyen Ly Minh Man <nguyenlyminhman@gmail.com>
2018-11-07 23:16:30 +0700

    Add Repository

---
+++ src/main/java/com/taya/dao/production/CategoryRepository.java
@@ -0,0 +1,14 @@
+/**
+ * TAYAFOOD 2018
+ */
+package com.taya.dao.production;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * @author ManNLM
+ */
+@RepositoryRestResource
+public interface CategoryRepository {
+
+}
---
+++ src/main/java/com/taya/dao/production/ImageRepository.java
@@ -0,0 +1,15 @@
+/**
+ * TAYAFOOD 2018
+ */
+package com.taya.dao.production;
+
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+/**
+ * @author ManNLM
+ */
+
+@RepositoryRestResource
+public class ImageRepository {
+
+}
